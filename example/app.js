6ff6bd0625918819b410cda6c123d2499f7a31c8
kamiyaU <fuzzy31u@gmail.com>
2015-01-08 15:25:30 +0900

    check validations.

--- example/app.js
+++ example/app.js
@@ -14,9 +14,9 @@ var specs = {
     schema: {
       type: 'object',
       properties: {
-        userId: 'string',
-        firstName: 'string',
-        lastName: 'string',
+        userId: { type: 'number', maxLength: 100, minLength: 1},
+        firstName: { type: 'string', maxLength: 100, minLength: 1},
+        lastName: { type: 'string', maxLength: 100, minLength: 1},
         email: { type: 'string', style: 'long' },
         phone: 'string'
       },
@@ -42,8 +42,8 @@ var specs = {
     schema: {
       type: 'object',
       properties: {
-        companyId: 'string',
-        name: 'string',
+        companyId: { type: 'string', maxLength: 100, minLength: 1},
+        name: { type: 'string', maxLength: 100, minLength: 1},
         phrase: 'string',
         country: 'string',
         zipCode: 'string',
