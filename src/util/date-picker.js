6a66a640cd3b111ca22b066cdcd888c983c30ee1
kamiyaU <fuzzy31u@gmail.com>
2015-01-19 15:26:54 +0900

    Issue#17 Format date to ISO8601 with original datepicker.js

---
+++ src/util/date-picker.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  var app;
+
+  app = angular.module('datePicker', ['mgcrea.ngStrap', 'ngMaterial', 'ui.utils']);
+
+  app.directive('mdDatePicker', [    
+    '$datepicker', function($datepicker) {      
+      return {
+        template: '<div class="md-toolbar-tools dark">' + ' <md-input-container ng-disabled="isDisabled">' + '     <label>{{label}}</label>' + '    <input data-date-format="yyyy-MM-dd" id="date-picker" model-view-value="true"' + '             ng-model="value" bs-datepicker></md-input>' + '</md-input-container>' + '</div>',
+        restrict: 'E',
+        scope: {
+          lineColor: "@",
+          textColor: "@",
+          mask: "@"
+        },
+        link: function(scope, element, attrs) {
+          var picker;
+          picker = $("#date-picker");
+          if (scope.textColor) {
+            picker.css("color", scope.textColor, "important");
+          }
+          if (scope.lineColor) {
+            picker.css("border-color", scope.lineColor, "important");
+          }
+        }
+      };
+    }
+  ]);
+
+}).call(this);
