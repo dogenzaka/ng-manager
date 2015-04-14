eb99d4cc2eb7332032225445c45940cee47e3ebc
CS0003 <shimosaka_kimihiko@amesta.co.jp>
2015-04-14 10:06:45 +0900

    fix TagInput

--- src/tag/tag-controller.js
+++ src/tag/tag-controller.js
@@ -2,10 +2,11 @@ angular
 .module('ngManager')
 .controller('TagCtrl', function($scope, $http, $apiService) {
   $scope.loadTags = function($query) {
-    return $apiService.get($scope.schema.tagUri).then(function(res) {
+    return $apiService.get($scope.schema.tagUri, {query: $query}).then(function(res) {
       return res.filter(function(value) {
         return value.text.toLowerCase().indexOf($query.toLowerCase()) != -1;
       });
     });
   };
 });
+
